6fcde0f52e02d664dc113ce4edb105a2776c8aec
Noemi Enguídanos <zygaena23@gmail.com>
2021-11-24 20:55:28 +0100

    Merge magia into mas_alla
    Conflicts:
    	public_html/datos/viaje.txt

---
+++ CFGS DAW IES S.OCHOA ELX/2º 2021-2022/ASIGNATURAS 1º/Programación/UNIDAD 3. UTILIZACIÓN DE OBJETOS/PracticaPOO/Proyectofuturo/src/proyectofuturo/Proyectofuturo.java
@@ -0,0 +1,28 @@
+package proyectofuturo;
+
+public class Proyectofuturo {
+
+    
+    public static void main(String[] args) {
+      
+        System.out.println("Estudiante " + args[0]);
+        System.out.println("Numero de módulos en los que está matriculada: " + args[1]);
+        System.out.println("Número de horas semanales que dedica al estudio: " + args[2]);
+
+        System.out.println("\n ");
+        System.out.println(Proyectofuturo.veredicto(args[1],args[2]));
+        System.out.println("\n ");
+}
+
+    public static String veredicto(String valor1, String valor2){
+    if (valor1.equals(valor2)) {
+    return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+}
+if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+    return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+}
+return "Ideal. Trabajas los contenidos en casa.";
+
+    }
+
+}
